2a2690d2804f8bc96defa98346b2aa80a9fae16b
VANDO MONTEIRO <vando.monteiro@etec.sp.gov.br>
2022-03-18 10:56:49 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1100_par_ou_impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Definir se valor é par ou impar");
+            Console.WriteLine();
+
+            Console.Write("digite o valor: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            if (a % 2 == 0)
+            {
+
+                Console.WriteLine(a + " é par!!!");
+
+            }
+            else
+            {
+                Console.WriteLine(a + " é ímpar!!!");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
